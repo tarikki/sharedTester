8bb199e32ba39422827c0e5906b09b3a7d0ce7bc
Pepe Lehtonen <pepe.lehtonen@gmail.com>
2014-11-02 20:25:23 +0100

    Te

--- Tester.java
+++ Tester.java
@@ -4,6 +4,7 @@
 public class Tester {
     public static void main(String[] args) {
         System.out.println("Kakki pois!");
-System.out.println("Kakki pois!");
+        System.out.println("Kakki pois!");
+        System.out.println("Paskaaaaaa");
     }
 }
