4366335c7a02a3049c8eec09df96a10de82d6c4e
extradikke <tariq.alsadoon@gmail.com>
2014-11-02 17:54:45 +0100

    version whatevah

--- Tester.java
+++ Tester.java
@@ -3,6 +3,6 @@
  */
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("whatever");
+        System.out.println("whatever, extra dikke yo yo");
     }
 }
